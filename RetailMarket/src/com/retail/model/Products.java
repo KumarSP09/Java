6333b462693ce50e9c1746a848c096ec821ffe7f
KumarSP <KumarSP@Shiva>
2018-10-23 23:12:20 +0530

    Product

--- RetailMarket/src/com/retail/model/Products.java
+++ RetailMarket/src/com/retail/model/Products.java
@@ -2,4 +2,42 @@ package com.retail.model;
 
 public class Products {
 
+	private String ProductCategory;
+	private String ProductSubCategory;
+	private String ProductName;
+	private int Quantity;
+	private double rate;
+	
+	
+	public String getProductCategory() {
+		return ProductCategory;
+	}
+	public void setProductCategory(String productCategory) {
+		ProductCategory = productCategory;
+	}
+	public String getProductSubCategory() {
+		return ProductSubCategory;
+	}
+	public void setProductSubCategory(String productSubCategory) {
+		ProductSubCategory = productSubCategory;
+	}
+	public String getProductName() {
+		return ProductName;
+	}
+	public int getQuantity() {
+		return Quantity;
+	}
+	public void setQuantity(int quantity) {
+		Quantity = quantity;
+	}
+	public double getRate() {
+		return rate;
+	}
+	public void setRate(double rate) {
+		this.rate = rate;
+	}
+	public void setProductName(String productName) {
+		ProductName = productName;
+	}
+	
 }
